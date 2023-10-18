20c3c12e08934353b7f9b481cde1aa31ab83c56b
Portatiles Biblioteca <portbiblioteca@academico.unab.ad>
2023-10-18 18:33:04 -0500

    b

--- src/schemas/award.schema.js
+++ src/schemas/award.schema.js
@@ -13,16 +13,16 @@ const reclaimAwardSchema = object({
   id_reclamante: string(),
 });
 
-const coste_premio = number().min(0);
-const estado_premio = boolean();
+const coste_premioSchema = number().min(0);
+const estado_premioSchema = boolean();
 
 export const validateAwardSchema = (award) => awardSchema.safeParse(award);
 
 export const validateCoste_premio = (coste_premio) =>
-  coste_premio.safeParse(coste_premio);
+  coste_premioSchema.safeParse(coste_premio);
 
 export const validateEstado_premio = (estado_premio) =>
-  estado_premio.safeParse(estado_premio);
+  estado_premioSchema.safeParse(estado_premio);
 
 export const validateReclaimAwardSchema = (reclaimAward) =>
   reclaimAwardSchema.safeParse(reclaimAward);
